694161ba3936322a6da5e245af40ee6058e2b786
Sasa Mora Roca <sabata79@hotmail.com>
2023-09-24 13:48:00 +0300

    Gifted chat training

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { replies } from './assets/replies';
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Welcome to the chat! Im here to answer random replies from the list.',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Bot',
+          avatar: require('./assets/roBot.png'),
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+    let newMessage = {
+      _id: new Date().valueOf(),
+      text: replies[Math.floor(Math.random() * replies.length)],
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'Bot',
+        avatar: require('./assets/roBot.png'),
+      },
+    };
+    setMessages(previousMessages => GiftedChat.append(previousMessages, [newMessage]));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+        name: 'User',
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
